98307c3925f23abd83b1bf1decf5ad4d1b606c9b
Angelixus <angelixus99@gmail.com>
2020-03-07 18:28:35 +0100

    Created basic templates and controllers

--- sdi1920-entrega1-1920-310-1920-313/src/main/java/com/uniovi/controllers/UserController.java
+++ sdi1920-entrega1-1920-310-1920-313/src/main/java/com/uniovi/controllers/UserController.java
@@ -1,5 +1,17 @@
 package com.uniovi.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.uniovi.entities.User;
+
+@Controller
 public class UserController {
 
+	@RequestMapping(value="/signup")
+	public String signup(Model model) {
+		model.addAttribute("user", new User());
+		return "signup";
+	}
 }
