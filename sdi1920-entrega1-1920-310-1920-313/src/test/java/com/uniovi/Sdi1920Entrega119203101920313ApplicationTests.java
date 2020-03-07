f01bc3fc5c06d82258e121edfe557d8742ef220c
Angelixus <angelixus99@gmail.com>
2020-03-07 16:44:04 +0100

    Initial Skeleton

---
+++ sdi1920-entrega1-1920-310-1920-313/src/test/java/com/uniovi/Sdi1920Entrega119203101920313ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniovi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sdi1920Entrega119203101920313ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
