96a67fa49544f6a675f34024b35bde7bac682ebb
Ángel Olmedo García <angelixus99@gmail.com>
2020-03-11 09:20:59 +0100

    Removed unused imports

--- sdi1920-entrega1-1920-310-1920-313/src/main/java/com/uniovi/repositories/FriendRequestRepository.java
+++ sdi1920-entrega1-1920-310-1920-313/src/main/java/com/uniovi/repositories/FriendRequestRepository.java
@@ -1,13 +1,8 @@
 package com.uniovi.repositories;
 
-import javax.transaction.Transactional;
-
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.uniovi.entities.FriendRequest;
-import com.uniovi.entities.User;
 
 public interface FriendRequestRepository extends CrudRepository<FriendRequest, Long>{
 
